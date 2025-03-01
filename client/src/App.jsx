947d8c6e0a6bad627a5d8e8d46374568b7a698ff
gouri garg <garggauri661@gmail.com>
2025-03-01 19:55:50 +0530

    MonthlyReport

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 //import Student from './Components/StudentDashBoard/Student'
 //import OccasionForm from "./Components/OccasionForm/tempOccasion"; // Correct import
 //import Studentoccasion from "./Components/Studentoccasion/Studentoccasion";
-import Stdreg from "./Components/Registration/Stdreg";
+import MonthlyReport from "./Components/MonthlyReport/MonthlyReport";
+//import ResetPass from "./Components/ResetPassword/ResetPass";
+//import Stdreg from "./Components/Registration/Stdreg";
 function App() {
   return (
     <>
-      <Stdreg/>
+    <MonthlyReport/>
     </>
   );
 }
